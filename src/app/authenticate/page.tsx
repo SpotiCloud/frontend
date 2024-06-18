01a8746ded3e2786f5cd56295b15e6a1f1e5a1ab
KevinOomen <kevin.oomen@student.fontys.nl>
2024-06-18 02:53:48 +0200

    emergency fix

--- src/app/authenticate/page.tsx
+++ src/app/authenticate/page.tsx
@@ -4,24 +4,15 @@ import { getSession, signIn } from "next-auth/react";
 import { useEffect } from "react";
 
 export default async function Home(req) {
-  const session = await getSession({ req });
+  /*const session = await getSession({ req });
 
   const accessToken = session.token.accessToken;
-  console.log(accessToken);
+  console.log(accessToken);*/
   //const { data: session } = useSession();
 
   return (
     <div>
-      { session ? 
-        <button>
-          {session.token.accessToken}
-          Logout
-        </button>
-      : 
-        <button onClick={() => signIn("keycloak")}>
-          Login
-        </button>
-      }
+      
     </div>
   );
 }
